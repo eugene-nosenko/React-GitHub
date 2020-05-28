c0909d548c60fa3fbba831e6293030d06555c750
eugene-finn <1287175@gmail.com>
2020-05-28 17:30:20 +0300

    add BrowserRouter, Switch, Route in App.js. And NavLink Navbar.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,14 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Информация</h1>
+        <p className="lead">
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </div>
     </div>
   );
 };
